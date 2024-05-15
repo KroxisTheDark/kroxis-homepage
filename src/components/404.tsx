0977d4f0bc5f21b9ce9c584c6ab687cfc959ce2d
Vladislav Zhukov <officialzhukov@gmail.com>
2024-05-15 13:16:15 +0300

    little changes

---
+++ src/components/404.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    <>
+    return (
+        <div>Not Found</div>
+        <div>There is no page you're looking for.</div>
+        <p>May the Force will guide you</p>
+    )
+    </>
+}
+
+export default NotFound;
